0c2f96ea5a170b9eb607686f86807b5f5451171b
Vinicius Machado <dev.viniciusmachado@gmail.com>
2024-06-02 11:28:15 +0100

    publish

---
+++ dist/assets/BlankLayout-18b2a0ce.js
@@ -0,0 +1,1 @@
+import{v as e,o as a,f as t,w as o,b as s,y as r,a8 as n,a9 as c}from"./index-b45d696c.js";const l=e({setup(p){return(u,f)=>(a(),t(c,null,{default:o(()=>[s(r(n))]),_:1}))}});export{l as default};
