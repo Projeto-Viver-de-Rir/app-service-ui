3a5ca054cb617e217f9c56dac6eec5901af04f75
Sara Borges <sarakborges@outlook.com>
2024-06-19 21:49:59 -0300

    made administrative working groups actually load

--- src/stores/workingGroupStore.ts
+++ src/stores/workingGroupStore.ts
@@ -1,76 +1,79 @@
-import { fa } from 'vuetify/lib/locale/index.mjs'
-import { container } from 'tsyringe'
-import { defineStore } from 'pinia'
-import { computed, reactive } from 'vue'
+import { fa } from "vuetify/lib/locale/index.mjs";
+import { container } from "tsyringe";
+import { defineStore } from "pinia";
+import { computed, reactive } from "vue";
 
-import type { team } from '../entities/team'
-import { teamRepository } from '../repositories/teamRepository'
+import type { team } from "../entities/team";
+import { teamRepository } from "../repositories/teamRepository";
 
 interface workingGroupState {
-    workingGroups: team[],
-    isLoading: false,
-  }
+  workingGroups: team[];
+  isLoading: false;
+}
 
-export const useWorkingGroups = defineStore('workingGroups', () => {
+export const useWorkingGroups = defineStore("workingGroups", () => {
   const state = reactive<workingGroupState>({
-      workingGroups: [],
-      isLoading: false
-  })
+    workingGroups: [],
+    isLoading: false,
+  });
 
   const repository = container.resolve(teamRepository);
-  const isLoading = computed(() => state.isLoading)
+  const isLoading = computed(() => state.isLoading);
 
   const searchFiscal = async () => {
-    state.isLoading = true
-    
-    const data = await repository.search("", "1", 1, 15)
+    state.isLoading = true;
+
+    const data = await repository.search("", "1", 1, 15);
     state.workingGroups = data.result;
 
     state.isLoading = false;
-  }
+  };
 
   const searchAdvisory = async () => {
-    state.isLoading = true
-    
-    const data = await repository.search("", "2", 1, 15)
+    state.isLoading = true;
+
+    const data = await repository.search("", "2", 1, 15);
     state.workingGroups = data.result;
 
     state.isLoading = false;
-  }
+  };
 
   const searchLegal = async () => {
-    state.isLoading = true
-    
-    const data = await repository.search("", "3", 1, 15)
+    state.isLoading = true;
+
+    const data = await repository.search("", "3", 1, 15);
     state.workingGroups = data.result;
 
     state.isLoading = false;
-  }
+  };
 
   const searchOperational = async () => {
-    state.isLoading = true
-    
-    const data = await repository.search("", "4", 1, 15)
+    state.isLoading = true;
+
+    const data = await repository.search("", "4", 1, 15);
     state.workingGroups = data.result;
 
     state.isLoading = false;
-  }
+  };
 
   const searchAdministrative = async () => {
-    state.isLoading = true
-    
-    const data = await repository.search("", "5", 1, 15)
+    state.isLoading = true;
+
+    const data = await repository.search("", "5", 1, 15);
     state.workingGroups = data.result;
 
     state.isLoading = false;
-  }
+  };
+
+  const getAdministrative = computed(() => state.workingGroups);
 
   return {
-      searchFiscal,
-      searchAdvisory,
-      searchLegal,
-      searchOperational,
-      searchAdministrative,
-      isLoading
-  }
+    searchFiscal,
+    searchAdvisory,
+    searchLegal,
+    searchOperational,
+    searchAdministrative,
+    getAdministrative,
+    isLoading,
+  };
 });
--- src/views/volunteers/List.vue
+++ src/views/volunteers/List.vue
@@ -1,89 +1,94 @@
 <script lang="ts">
-import { ref,defineComponent,onMounted,computed } from 'vue';
-import type {ComputedRef, Ref} from 'vue'
+import { ref, defineComponent, onMounted, computed } from "vue";
+import type { ComputedRef, Ref } from "vue";
 
-import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
-import UiParentCard from '@/components/shared/UiParentCard.vue';
-import volunteersTable from '@/components/tables/volunteersTable.vue';
+import BaseBreadcrumb from "@/components/shared/BaseBreadcrumb.vue";
+import UiParentCard from "@/components/shared/UiParentCard.vue";
+import volunteersTable from "@/components/tables/volunteersTable.vue";
 
-import {useRoute, useRouter} from 'vue-router'
-import { useVolunteers } from '../../stores/volunteerStore'
+import { useRoute, useRouter } from "vue-router";
+import { useVolunteers } from "../../stores/volunteerStore";
 
 interface SetupData {
-    filters: ComputedRef;
-    getUsers: () => void;
-    isLoading: ComputedRef<bool>
+  filters: ComputedRef;
+  getUsers: () => void;
+  isLoading: ComputedRef<bool>;
 }
 
 export default defineComponent({
-    name: 'volunteerList',
-    components: {
-        BaseBreadcrumb,
-        UiParentCard,
-        volunteersTable,
-    },
-    setup(): SetupData {
-        const page = ref({ title: 'Voluntários' });
-        const breadcrumbs = ref([
-            {
-                text: 'Dashboard',
-                disabled: false,
-                href: '/'
-            },
-            {
-                text: 'Voluntários',
-                disabled: true,
-                href: '#'
-            }
-        ]);
-        const route = useRoute()
-        const router = useRouter()
-        const store = useVolunteers()
-        const filters = computed(() => store.filters);
-        const isLoading = computed(() => store.isLoading);
-        const getUsers = (): void => {
-            store.filter()
-        }
+  name: "volunteerList",
+  components: {
+    BaseBreadcrumb,
+    UiParentCard,
+    volunteersTable,
+  },
+  setup(): SetupData {
+    const page = ref({ title: "Voluntários" });
+    const breadcrumbs = ref([
+      {
+        text: "Dashboard",
+        disabled: false,
+        href: "/",
+      },
+      {
+        text: "Voluntários",
+        disabled: true,
+        href: "#",
+      },
+    ]);
+    const route = useRoute();
+    const router = useRouter();
+    const store = useVolunteers();
+    const filters = computed(() => store.filters);
+    const isLoading = computed(() => store.isLoading);
+    const getUsers = (): void => {
+      store.filter();
+    };
 
-        return {
-            filters,
-            getUsers,
-            page,
-            breadcrumbs,
-            isLoading
-        }
-    }
-})
-
+    return {
+      filters,
+      getUsers,
+      page,
+      breadcrumbs,
+      isLoading,
+    };
+  },
+});
 </script>
 
 <template>
-    <div v-if="isLoading" class="loading"></div>
-    <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
-    <v-row>
-        <v-col cols="12" md="12">
-            <UiParentCard title="Filtros">  
-                <v-row>
-                    <v-col cols="5" md="5">
-                            <v-text-field label="Nome" v-model="filters.name">
-                                
-                            </v-text-field>
-                    </v-col>
-                    <v-col cols="5" md="5">
-                            <v-text-field label="Nickname" v-model="filters.nickname">
-                                
-                            </v-text-field>
-                    </v-col>
-                    <v-col cols="2" md="2">
-                        <v-btn size="large" @click="getUsers()" color="primary" type="submit">Filtrar</v-btn>
-                    </v-col>
-                </v-row>
-            </UiParentCard>
-        </v-col>
-    </v-row>
-    <v-row>
-        <v-col cols="12" md="12">
-            <volunteersTable></volunteersTable>
-        </v-col>
-    </v-row>
+  <div v-if="isLoading" class="loading"></div>
+  <BaseBreadcrumb
+    :title="page.title"
+    :breadcrumbs="breadcrumbs"
+  ></BaseBreadcrumb>
+  <v-row>
+    <v-col cols="12" md="12">
+      <UiParentCard title="Filtros">
+        <v-row>
+          <v-col cols="5" md="5">
+            <v-text-field label="Nome" v-model="filters.name"> </v-text-field>
+          </v-col>
+          <v-col cols="5" md="5">
+            <v-text-field label="Nickname" v-model="filters.nickname">
+            </v-text-field>
+          </v-col>
+          <v-col cols="2" md="2">
+            <v-btn
+              size="large"
+              @click="getUsers()"
+              color="primary"
+              type="submit"
+              >Filtrar</v-btn
+            >
+          </v-col>
+        </v-row>
+      </UiParentCard>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12" md="12">
+      <volunteersTable></volunteersTable>
+    </v-col>
+  </v-row>
 </template>
