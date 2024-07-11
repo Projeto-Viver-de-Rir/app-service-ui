2ddb8b7e591a6f986e2237d6b48d0030308cf162
Sara Borges <sarakborges@outlook.com>
2024-07-11 18:46:26 -0300

    added summary tag

--- src/components/tables/personalDebtsTable.vue
+++ src/components/tables/personalDebtsTable.vue
@@ -43,6 +43,41 @@ export default defineComponent({
 });
 </script>
 
+<style>
+.year {
+  summary {
+    padding: 8px 16px;
+    cursor: pointer;
+
+    > h3 {
+      display: inline;
+
+      padding: 0 8px;
+    }
+  }
+}
+
+.month-table > .v-table__wrapper > table > :is(tbody, thead) {
+  tr > * {
+    &:not(:nth-child(2)) {
+      width: 15%;
+    }
+
+    &:not(:nth-child(1)) {
+      padding: 0 !important;
+    }
+
+    &:nth-child(1) {
+      padding: 0 16px !important;
+    }
+
+    &:nth-child(2) {
+      width: 30%;
+    }
+  }
+}
+</style>
+
 <template>
   <v-row>
     <v-card class="border" elevation="0">
@@ -56,59 +91,63 @@ export default defineComponent({
             <th class="text-h6">Pagamento</th>
           </tr>
         </thead>
-
-        <tbody>
-          <template v-for="year in Object.keys(debts.results)" :key="year">
-            <tr>
-              <td colspan="5">{{ year }}</td>
-            </tr>
-
-            <tr
-              v-for="item in debts.results[year]"
-              :key="item.id"
-              class="month-item"
-            >
-              <td></td>
-
-              <td>
-                <div class="d-flex align-left">
-                  <div>
-                    <h6 class="text-h6">{{ item.volunteer.name }}</h6>
-                  </div>
-                </div>
-              </td>
-
-              <td>
-                <div class="d-left">
-                  <div>
-                    <h6 class="text-h6">
-                      {{ formatDate(new Date(item.dueDate)) }}
-                    </h6>
-                  </div>
-                </div>
-              </td>
-
-              <td>
-                <div class="d-left">
-                  <div>
-                    <h6 class="text-h6">
-                      {{ formatCurrency(item.amount) }}
-                    </h6>
-                  </div>
-                </div>
-              </td>
-
-              <td>
-                <div class="d-left">
-                  <div>
-                    <h6 v-if="item.value !== null" class="text-h6"></h6>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </template>
-        </tbody>
       </v-table>
+
+      <template v-for="year in Object.keys(debts.results)" :key="year">
+        <details class="year" open>
+          <summary>
+            <h3>{{ year }}</h3>
+          </summary>
+
+          <v-table class="month-table">
+            <tbody>
+              <tr
+                v-for="item in debts.results[year]"
+                :key="item.id"
+                class="month-item"
+              >
+                <td></td>
+
+                <td>
+                  <div class="d-flex align-left">
+                    <div>
+                      <h6 class="text-h6">{{ item.volunteer.name }}</h6>
+                    </div>
+                  </div>
+                </td>
+
+                <td>
+                  <div class="d-left">
+                    <div>
+                      <h6 class="text-h6">
+                        {{ formatDate(new Date(item.dueDate)) }}
+                      </h6>
+                    </div>
+                  </div>
+                </td>
+
+                <td>
+                  <div class="d-left">
+                    <div>
+                      <h6 class="text-h6">
+                        {{ formatCurrency(item.amount) }}
+                      </h6>
+                    </div>
+                  </div>
+                </td>
+
+                <td>
+                  <div class="d-left">
+                    <div>
+                      <h6 v-if="item.value !== null" class="text-h6"></h6>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+        </details>
+      </template>
     </v-card>
   </v-row>
 </template>
