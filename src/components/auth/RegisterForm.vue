1f0990d964e87e73f8185de64ea2d80a31e0c228
Luiza Haas <luizahaas2@gmail.com>
2024-08-01 08:55:43 -0300

    feat: success message

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -15,16 +15,21 @@ const emailRules = ref([
   (v: string) => /.+@.+\..+/.test(v) || "Insira um e-mail válido",
 ]);
 
+const confirmed = ref(false);
+
 async function validate(values: any, { setErrors }: any) {
   const authStore = useAuthStore();
 
   await authStore
   .register(email.value, password.value)
     .then(({ response }) => {
-      if (response?.data?.status !== 200) {
-        const { errors } = response.data;
-        setErrors({ apiError: errors })
+      if (response?.status === 200) {
+        confirmed.value = true;
+        return;
       }
+
+      const errors = response?.data?.errors || "Ocorreu um erro geral.";
+      setErrors({ apiError: errors });
     })
     .catch((error) => setErrors({ apiError: error }));
 }
@@ -48,6 +53,7 @@ async function validate(values: any, { setErrors }: any) {
     ></VTextField>
     <v-btn size="large"
       :loading="isSubmitting"
+      :disabled="confirmed"
       color="primary"
       block
       type="submit"
@@ -56,12 +62,16 @@ async function validate(values: any, { setErrors }: any) {
     >
     <div v-if="errors.apiError" class="mt-2">
       <v-alert color="error">
-        Não foi possível criar seu cadastro. Motivos:
+        Não foi possível criar seu cadastro. Motivo(s):
         <p class="pl-5" v-if="typeof errors.apiError === 'object'" v-for="(error, i) in errors.apiError" :key="i">
           <span v-if="Array.isArray(error)" v-for="(message) in error" :key="i">{{ message }}</span>
         </p>
         <p class="pl-5" v-else>{{ errors.apiError }}</p>
       </v-alert>
     </div>
+
+    <v-alert color="success" v-if="confirmed">Seu cadastro foi realizado! <br />
+      Em breve você deverá receber um e-mail para continuar seu cadastro.
+    </v-alert>
   </Form>
 </template>
