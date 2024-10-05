47b199456c4caa82bcaa79b66cbdd100ee1fa942
Caio Silveira <caiocsilveira@gmail.com>
2024-10-05 22:46:32 +0100

    fix: refactor cleanse on event components/routes/store + tests

--- src/router/eventsRoutes.ts
+++ src/router/eventsRoutes.ts
@@ -12,11 +12,6 @@ const eventsRoutes = {
       component: () => import("@/views/events/EventList.vue"),
     },
     {
-      name: "DetailtEvent",
-      path: "/events/Detalhes/:id",
-      component: () => import("@/views/events/Detail.vue"),
-    },
-    {
       name: "EventDetails",
       path: "/events/details/:id",
       component: () => import("@/views/events/EventDetails.vue"),
@@ -30,21 +25,6 @@ const eventsRoutes = {
       name: "EventForm",
       path: "/events/form/:id?",
       component: () => import("@/views/events/EventForm.vue"),
-    },
-    {
-      name: "CreateEvent",
-      path: "/events/Adicionar",
-      component: () => import("@/views/events/Detail.vue"),
-    },
-    {
-      name: "ManageVolunteers",
-      path: "/events/Detalhes/:id/Voluntarios",
-      component: () => import("@/views/events/ManageVolunteers.vue"),
-    },
-    {
-      name: "FinishVisit",
-      path: "/events/Detalhes/:id/Concluir",
-      component: () => import("@/views/events/FinishVisit.vue"),
     },
   ],
 };
